136f183e88bdc04abc5a81845537cfeb64761d64
Ankit Ghosh <38413606+Ankit006@users.noreply.github.com>
2023-12-25 18:59:10 +0530

    Revert "Revert "added company create and fetch logic""

---
+++ src/plugins/company/company.plugin.ts
@@ -0,0 +1,15 @@
+import { DoneFuncWithErrOrRes, FastifyInstance } from "fastify";
+import { CompanyPluginOptionType } from "../../types";
+import { companyRoutes } from "./company.route";
+export default function companyPlugin(
+  fastifyInstance: FastifyInstance,
+  _opts: CompanyPluginOptionType,
+  done: DoneFuncWithErrOrRes
+) {
+  companyRoutes(fastifyInstance);
+
+  fastifyInstance.register(require("./members/company_members.plugin"), {
+    prefix: "/:companyId",
+  });
+  done();
+}
