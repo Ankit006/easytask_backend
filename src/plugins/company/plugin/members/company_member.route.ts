ab7144d068269bb379097030fc036999923990ff
Ankit006 <ankitghosh311@outlook.com>
2023-12-26 02:55:36 +0530

    fix header bugs

--- src/plugins/company/plugin/members/company_member.route.ts
+++ src/plugins/company/plugin/members/company_member.route.ts
@@ -5,15 +5,19 @@ import { IUser } from "../../../../database/database.schema";
 
 export function companyMemberRoute(fastify: FastifyInstance) {
   fastify.get("/header", async function (request, reply) {
-    const user = await this.DBClient.userCollection().findOne<IUser>({
-      _id: new ObjectId(request.userId),
-    });
+    const user = await this.DBClient.userCollection().findOne<IUser>(
+      {
+        _id: new ObjectId(request.userId),
+      },
+      { projection: { password: 0 } }
+    );
     if (user) {
-      return reply
-        .send(HttpStatus.SUCCESS)
-        .send({ ...user, role: request.memberRole });
+      const headerData = { ...user, role: request.memberRole };
+      return reply.status(HttpStatus.SUCCESS).send(headerData);
     } else {
-      return reply.send(HttpStatus.NOT_FOUND).send({ error: "User not found" });
+      return reply
+        .status(HttpStatus.NOT_FOUND)
+        .send({ error: "User not found" });
     }
   });
 }
